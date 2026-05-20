07dee6c00080840fab27fc18bb10b12a5788c956
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-05-20 15:32:29 +0300

    create/join session by name

--- Assets/Scripts/SessionJoiner.cs
+++ Assets/Scripts/SessionJoiner.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 public class SessionJoiner : MonoBehaviour
 {
     [SerializeField] private NetworkRunner networkRunner;
+    public string SessionName { get; set; }
     
     public void TestCreateSession()
     {
         networkRunner.StartGame(new StartGameArgs()
         {
             GameMode = GameMode.Shared,
-            SessionName = "TestSession",
+            SessionName = SessionName,
             CustomLobbyName = LobbyJoiner.Instance.LobbyName
         });
+    }
+
+    public static string SessionNameCreator()
+    {
+        return Random.Range(0, 1000000).ToString();
     }
 }
