f8257d589ebf12ac42261a78b6f35d00f3fef4ad
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-05-22 16:49:10 +0300

    can't join lobby while waiting to join

--- Assets/Scripts/LobbyJoiner.cs
+++ Assets/Scripts/LobbyJoiner.cs
@@ -7,23 +7,37 @@ public class LobbyJoiner : PersistentSingleton<LobbyJoiner>
 {
     [SerializeField] private NetworkRunner networkRunner;
     public UnityEvent OnJoinedLobby;
+    public UnityEvent OnStartJoin;
+    public UnityEvent OnCancelJoin;
     [field: SerializeField]
     public string LobbyName { get; private set; }
+    private bool busy;
 
     public void JoinLobby(string lobbyName)
     {
+        if (busy) return;
         JoinLobbyAsync(lobbyName);
     }
     
     private async Task JoinLobbyAsync(string lobbyName)
     {
-        var result =  await networkRunner.JoinSessionLobby(SessionLobby.Custom, lobbyName);
+        busy = true;
         
+        OnStartJoin.Invoke();
+        
+        var result =  await networkRunner.JoinSessionLobby(SessionLobby.Custom, lobbyName);
         
         if (result.Ok)
         {
             LobbyName = lobbyName;
             OnJoinedLobby.Invoke();
         }
+        else
+        {
+            print(result.ShutdownReason);
+            OnCancelJoin.Invoke();
+        }
+        
+        busy = false;
     }
 }
