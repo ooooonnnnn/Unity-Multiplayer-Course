d8ba78cc2fd59b45bb97c7310131e029d4c168d6
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-06-12 23:16:00 +0300

    scene authority can start game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,31 @@
+using System;
+using Fusion;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : PersistentSingleton<GameManager>
 {
+    [SerializeField] private SceneAsset gameSceneAsset;
+
     public void QuitGame()
     {
         print("Quitting game");
         Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        NetworkRunner runner = SinglePeer_NetworkRunnerManager.Instance.NetworkRunner;
+        if (!runner.IsSceneAuthority)
+        {
+            Debug.LogWarning("Cannot start game, not the scene authority");
+            return;
+        }
+
+        var loadAsyncOp = runner.LoadScene(gameSceneAsset.name, LoadSceneMode.Single);
+        
+        UIManager.Instance.ShowWaitingScreen();
     }
 }
