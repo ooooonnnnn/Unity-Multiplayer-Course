55912dd0acda58cf331a67f29baa10c93e2e9172
Eitan <romebattle.net@gmail.com>
2026-06-16 12:56:21 +0300

    Chat fixed overlap

--- Assets/Scripts/UI/Chat/Message.cs
+++ Assets/Scripts/UI/Chat/Message.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Message : MonoBehaviour
 {
     [SerializeField] private Text messageText;
+
+    private void Start()
+    {
+    }
 
     public void SetText(string text)
     {
